5c5a67330244453fca85057fff9bccb152581f62
Spencer Alexander <spencer.r.alexander@gmail.com>
2013-06-25 10:04:52 -0600

    Updating title to have eventname

--- main.rb
+++ main.rb
@@ -7,5 +7,6 @@ end
 
 get '/:event' do
   halt 404, 'file not found' unless File.exists?("views/events/#{params[:event]}.erb")
+  @eventname = params[:event].gsub('-',' ').capitalize
   erb ('events/' + params[:event]).to_sym, :layout => :master
 end
