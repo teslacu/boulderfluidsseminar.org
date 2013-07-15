fe226f6684ab8f229780237ef9f88001540de088
phamlington <peterh@ecae158-86-dhcp.colorado.edu>
2013-07-15 16:46:12 -0600

    Edits to deploy and public

--- deploy.sh
+++ deploy.sh
@@ -11,20 +11,15 @@ do
   extension="${filename##*.}"
   filename="${filename%.*}"
   echo "Processing $filename file..."
-  mkdir ./public/$filename
-  curl localhost:4567/$filename > ./public/$filename/index.html
+  mkdir ../public/$filename
+  curl localhost:4567/$filename > ../public/$filename/index.html
 done
 
 # sync up the public directory github
-cd public
+cd ../public
+git pull
 git add *
 git diff --diff-filter=D --name-only -z | xargs -0 git rm
 git commit -m "updating live site"
 git push origin gh-pages
-
-# sync up the origin branch
-cd ..
-git add public
-git commit -m "updating live site"
-git push origin master
 
